4af1f076d7146e7849d18d1e36441d7487c63f04
Christoph Straßer <christoph.strasser78@gmail.com>
2020-08-18 18:24:05 +0200

    #17 DataTable: add licence header

--- primefaces-selenium-components/src/main/java/org/primefaces/extensions/selenium/component/DataTable.java
+++ primefaces-selenium-components/src/main/java/org/primefaces/extensions/selenium/component/DataTable.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2020 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.primefaces.extensions.selenium.component;
 
 import org.openqa.selenium.By;
@@ -74,7 +89,7 @@ public abstract class DataTable extends AbstractPageableData {
     public void sort(String headerText) {
         for (Cell cell : getHeader().getCells()) {
             if (cell.getText().equals(headerText)) {
-                cell.getWebElement().findElement(By.className("ui-column-title")).click();
+                cell.getWebElement().findElement(By.className("ui-sortable-column-icon")).click();
                 PrimeSelenium.waitGui().until(PrimeExpectedConditions.jQueryNotActive());
             }
         }
